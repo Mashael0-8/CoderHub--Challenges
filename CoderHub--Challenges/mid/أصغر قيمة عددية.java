76e66588900e3ba526905e92f2aa5d878a9798b5
Mashael0-8 <113108309+Mashael0-8@users.noreply.github.com>
2022-12-22 08:22:03 +0300

    CoderHub--Challenges
    1-Easy 1
    2-Mid
    3-Hard

---
+++ CoderHub--Challenges/mid/أصغر قيمة عددية.java
@@ -0,0 +1,12 @@
+public static int smallest_number(int[] arr) {
+    // write your code here
+
+    int result = arr[0];
+    for(int i =1; i<arr.length ; i++){
+        if(arr[i] < result)
+            result = arr[i];
+    }
+    return result;
+}
+Footer
+© 2022 GitHub, Inc.
